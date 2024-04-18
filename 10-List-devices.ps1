1826a6282bda3b01cf99d13f7027efaa726ba61a
ckarawani <ckarawani@goziro.com>
2024-04-18 11:47:22 -0400

    added example of device management

--- 10-List-devices.ps1
+++ 10-List-devices.ps1
@@ -1,11 +1,6 @@
 # ref. https://learn.microsoft.com/en-us/graph/api/teamworkdevice-list?view=graph-rest-beta&tabs=http
 
 
-# Using Powershell SDK
-Get-MgBetaTeamworkDevice | Format-List
-
-
-
 # Using REST
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
@@ -13,3 +8,12 @@ $headers.Add("Authorization", $graphToken)
 
 $response = Invoke-RestMethod 'https://graph.microsoft.com/beta/teamwork/devices' -Method 'GET' -Headers $headers
 $response | ConvertTo-Json
+
+
+# Using Powershell SDK
+Get-MgBetaTeamworkDevice | Ft @{label='User';e={$_.CurrentUser.DisplayName}}, @{label='Model';e={$_.HardwareDetail.Model}}, @{label='Manufacturer';e={$_.HardwareDetail.Manufacturer}}, HealthStatus
+
+
+
+# Get-MgBetaTeamworkDevice | ConvertTo-Json -Depth 2
+
--- 5-License-user.ps1
+++ 5-License-user.ps1
@@ -1,5 +1,7 @@
 $upnInput =  Read-Host -Prompt 'Comma Seperated List of Users to License' 
 $skuInput =  Read-Host -Prompt 'Comma Seperated List of Licenses to assign (Ex. MCOPSTNC,SPE_E5,)' 
+# MCOPSTNC,SPE_E5,Microsoft_Teams_Calling_Plan_pay_as_you_go_(country_zone_1)
+
 
 $upnArray = $upnInput.Split(",");
 $skuArray = $skuInput.split(",");
